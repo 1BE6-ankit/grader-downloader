923a924f1475ae90bd595964ffc6d7e2cbd0cf4b
Ankit Koirala <akoirala9@gmail.com>
2019-06-08 01:05:55 +0200

    HTML extraction refactored

--- src/index.py
+++ src/index.py
@@ -1,31 +1,14 @@
 import sys
 import re
-import urllib.request
-from urllib.parse import urlparse
 
 from scraper import GraderSite
 
 grader_url = sys.argv[1] if len(sys.argv)==2 \
     else "https://grader.eecs.jacobs-university.de/courses/320201/2019_1/"
 
-validated_url = urlparse(grader_url)
-if not (validated_url.scheme and validated_url.netloc):
-    print("Invalid url")
-    exit(1)
-
-print("For url: ", grader_url)
-
 # temp fix
 grader_url = "http://acalc.eu5.net"
 
-# create a request
-request = urllib.request.Request(grader_url)
-try:
-    response = urllib.request.urlopen(request) 
-except:
-    print("Cannot opne resource")
-    exit(1)
+grader = GraderSite(grader_url)
+grader.getHtmlContent()
 
-# get html content
-html_content = response.read().decode("utf8")
-print(html_content)
--- src/scraper.py
+++ src/scraper.py
@@ -1,3 +1,31 @@
-class GraderSite:
+import urllib.request
+from urllib.parse import urlparse
+
+class Site:
+    url = html_content = None
+    
     def __init__(self, url):
+        # first validate url
+        validated_url = urlparse(url)
+        if not (validated_url.scheme and validated_url.netloc):
+            print("Invalid url")
+            exit(1)
+
         self.url = url
+
+    def getHtmlContent(self):
+        # create a request
+        request = urllib.request.Request(self.url)
+        try:
+            response = urllib.request.urlopen(request) 
+        except:
+            print("Cannot opne resource")
+            exit(1)
+
+        # get html content
+        html_content = response.read().decode("utf8")
+        print(html_content)
+
+class GraderSite(Site):
+    def __init__(self, url):
+        Site.__init__(self, url)
