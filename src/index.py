16948eb3ade2f4892530b33c0b1792b783617dbe
Ankit Koirala <akoirala9@gmail.com>
2019-09-03 21:54:10 +0200

    Working with parameters changed for lecture download and assignment download. The identifier for lectures and assignment can be different based on the courses.

--- src/index.py
+++ src/index.py
@@ -4,9 +4,9 @@ import re
 from scraper import GraderSite
 
 grader_url = sys.argv[1] if len(sys.argv)==2 \
-    else "https://grader.eecs.jacobs-university.de/courses/320201/2019_1/"
+    else "https://grader.eecs.jacobs-university.de/courses/320112/2019_1gA/"
 
-# c
+# c II
     # else "https://grader.eecs.jacobs-university.de/courses/320112/2019_1gA/"
 
 # c++
--- src/scraper.py
+++ src/scraper.py
@@ -40,9 +40,14 @@ class GraderSite(Site):
         Site.get_html_content(self)
 
         # extract links 
-        # lecture = self.res_scraper("Course Materials")
-        lecture = self.res_scraper("Schedule")
-        assignments = self.res_scraper("Assignments")
+        lecture = self.res_scraper(" Course Materials: ")
+
+        # The parameter "Schedule:" should have no spaces
+        # lecture = self.res_scraper("Schedule:")
+
+        assignments = self.res_scraper(" Assignments: ")
+        # assignments = self.res_scraper(" Homework: ")
+
 
         download_path = "../resources/"
 
@@ -56,24 +61,35 @@ class GraderSite(Site):
 
     def download_links(self, download_path, links):
         for link in links:
-            content = urllib.request.urlopen(self.url+link[0])            
+            # There are two scenarios for a download link. 
+            # One condition is that the link may be an absolute directory from the grader url,
+            # OR it is a relative url from the course url. 
+            # Considering these two scenarios, we check if the link is absolute or not
+            download_url = self.url+link[0] if not link[0].startswith('/') \
+                else self.url[0:self.url.index("/courses")]+link[0]
+            
+            content = urllib.request.urlopen(download_url)            
             data = content.read()
             with open(download_path+link[1], 'w+b') as f:
                 f.write(data)
 
     def res_scraper(self, indentifier):
-        # extract the table holding the links 
-        base_string = "<tr><td class=menutext> {}: <\/td><td class=text>(.*?)</td></tr>".format(indentifier)
+        # Normally, each section is defined with a <tr><td class=menutext> ..... </td></tr>
+        # So, we extract this section defined by an additional 'identifier' to extract specific link
+        # section - assignment or lecture
+        base_string = "<tr><td class=menutext>{}<\/td><td class=text>(.*?)<\/td><\/tr><tr><td class=menutext>".format(indentifier)
         content_html = re.search(base_string, self.html_content).group(1)
 
-        print(content_html)
-
-        # extracts the link content between <li> </li>
-        content_lists = re.findall("<li>(.*?)</li>", content_html)
+        # get all the links. We leave '<' in closing </a> because this anchor tag contains a link 
+        # and a name such as: <a href=SOME_LINK>LINK_TEXT</a>. 
+        # If the '<' in </a> is included, findall cannot extract the LINK_TEXT
+        content_lists = re.findall("<a (.*?)\/a>", content_html)
 
         content_res = []
         for each_list in content_lists:
-            content_links = re.findall("href=(.*?)>(.*?)<\/a>", each_list)
+            # get the link and the link text. The link text is the file name after the file pointed
+            # by this link is extracted
+            content_links = re.findall("href=(.*?)>(.*?)<", each_list)
             content_res.append(content_links[0])
 
         return content_res
